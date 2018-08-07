78d1aced7082945708319ccf65e02c83d05a7b6f
emmalkl <emma_lkl@163.com>
2018-08-07 09:07:26 +0800

    webpack

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const glob = require('glob');
 const PurifyCSSPlugin = require('purifycss-webpack');
+const webpack=require("webpack");
+const entry=require('./webpack_config/webpack_entry.js');
+const CopyWebpackPlugin=require('copy-webpack-plugin');
 
 module.exports={
-    entry:'./src/index.js',
+    entry:entry,
     output:{
         filename:'bundle.js',
         path:path.resolve(__dirname,'dist')
@@ -41,6 +44,12 @@ module.exports={
                     fallback:"style-loader",
                     use:["css-loader",'sass-loader']
                 })
+            },{
+                test:/\.js$/,
+                use:{
+                  loader:"babel-loader",
+                  options:{presets:'env'}
+                }   
             }
         ]
     },
@@ -53,7 +62,18 @@ module.exports={
             },
             has:true,
             template:'./src/index.html'
-        })
+        }),
+        new PurifyCSSPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*.html'))
+        }),
+        new webpack.BannerPlugin("唯创网讯"),
+        new webpack.ProvidePlugin({
+            $:'jquery'
+        }),
+        new CopyWebpackPlugin([{
+            from: __dirname+'/src/public',
+            to:'./public'
+        }])
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
