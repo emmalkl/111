893f73720dc406838e3674b6576de27bec45bc66
emmalkl <emma_lkl@163.com>
2018-11-11 11:46:07 +0800

    vue

--- lx/demo/src/App.vue
+++ lx/demo/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div id="header"><h3>hello</h3></div>
+    <div id="nav"></div>
     <router-view/>
+    <div id="footer">
+      <router-link to="/">首页</router-link> 
+      <router-link to="/about">投票</router-link> 
+    </div>
   </div>
 </template>
 
@@ -16,5 +18,29 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 50px;
+  width: 100%;
+  background-color: red;
+  color: aliceblue;
+}
+#footer{
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 50px;
+  line-height: 50px;
+  width: 100%;
+  background-color: red; 
+}
+a{
+  color: aliceblue;
+  display: inline-block;
+  width: 25%;
+  text-decoration: none;
 }
 </style>
--- lx/demo/src/views/About.vue
+++ lx/demo/src/views/About.vue
@@ -1,5 +1,77 @@
 <template>
+<div class="nav">
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="top">
+      <img src="../../public/img/summer.png" alt="" width="20%" height="60%">
+      <h3>hi!</h3>
+    </div>
+    <div class="about-nav">
+      <ul>
+        <li v-for="(item,index) in objArr" :key="index"  @click="add(index)" v-bind:class="{active:item.isselected}">{{item.name}}</li>
+      </ul>
+    </div>
   </div>
+</div>
 </template>
+<style scoped>
+.top{
+  height: 100px;
+  width: 100%;
+}
+.top img{
+  margin-left: -70%;
+}
+.top h3{
+  margin-top: -20%;
+  margin-left: -10%;
+}
+.about-nav{
+  width: 100%;
+}
+ul{
+  margin-left: -40px;
+}
+li{
+  height: 30px;
+  list-style: none;
+  width: 100%;
+  border:1px solid red;
+  margin: 5px 0;
+}
+</style>
+<script>
+export default {
+  data(){
+    return{
+      objArr:[
+        {
+          id:1,
+          name:'选项一',
+          isselected:false,
+        },
+        {
+          id:2,
+          name:'选项二',
+          isselected:false,
+        },
+        {
+          id:3,
+          name:'选项三',
+          isselected:false,
+        },
+      ]
+    }
+  },
+  methods:{
+    add(index){
+      this.isselected=!isselected;
+      if(this.isselect){
+        var oli=document.getElementsByTagName("li");
+        oli[index].style.backgroundColor = "red";
+        
+      }    
+    },
+  }
+}
+</script>
+
